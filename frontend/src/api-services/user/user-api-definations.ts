ced31ef9a0f87c0eb351b5bb2e423c0d6045dd27
Arjun Nepali <nepaliarjun049@gmail.com>
2025-11-24 23:01:38 +0545

    feat:for user context

---
+++ frontend/src/api-services/user/user-api-definations.ts
@@ -0,0 +1,11 @@
+
+
+type userprofileresponse = {
+  id: string;
+  username: string;
+  email: string;
+  display_name?: string;
+  profile_picture?: string;
+};
+
+export type { userprofileresponse };
---
+++ frontend/src/context/UserContext.tsx
@@ -0,0 +1,8 @@
+
+type User ={
+    id:number;
+    username:string;
+    email:string;
+    display_name?:string;
+    profile_picture?:string;
+}
